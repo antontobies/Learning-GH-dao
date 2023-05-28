4157786302f3c085a1c099cbf463dad4ed192717
Anton Tobies <Anton.Tobies@tupana.de>
2023-05-28 09:28:00 +0200

    Initial Commit

---
+++ LearningGHdao/LearningGHdaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHdaoApp.swift
+//  LearningGHdao
+//
+//  Created by Anton Tobies on 28.05.23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHdaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
