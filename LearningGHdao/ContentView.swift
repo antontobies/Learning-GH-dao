ed89ecce60f34edac6660e273c4d9de85b5aff8b
antontobies <Anton.Tobies@tupana.de>
2023-07-02 18:26:58 +0200

    Update Text in ContentView.swift
    Change Text in TextField to "What's up, codecrew!"

--- LearningGHdao/ContentView.swift
+++ LearningGHdao/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, new world!")
+            Text("What's up, codecrew!")
             // comment inserted
             // another new comment
         }
