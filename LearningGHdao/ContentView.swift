0f34464ec80a0395300ef4eff0982fbceebfb3c7
Anton Tobies <Anton.Tobies@tupana.de>
2023-07-02 18:16:15 +0200

    add new file "MyModel.swift"

--- LearningGHdao/ContentView.swift
+++ LearningGHdao/ContentView.swift
@@ -21,6 +21,8 @@ struct ContentView: View {
     }
 }
 
+// another new comment here
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ LearningGHdao/MyModel.swift
@@ -0,0 +1,13 @@
+//
+//  MyModel.swift
+//  LearningGHdao
+//
+//  Created by Anton Tobies on 02.07.23.
+//
+
+import Foundation
+
+
+struct MyModel {
+    var name: String?
+}
